79993ad69cfd92c299fa2afcf00a66e231155406
Stéphane ZE-OGIER <contact@ze-ogier.com>
2020-08-17 16:53:25 +0200

    correct routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :travels do
-    resources :bookings, only [:new, :create]
+    resources :bookings, only: [:new, :create]
   end
 end
