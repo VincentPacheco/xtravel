6d239f064afbb174420e087435d4f4d9cca73550
Vincent Pacheco <vincentpacheco@outlook.com>
2020-08-17 16:22:36 +0200

    Models done.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
-
+  skip_before_action :authenticate_user!, raise: false, only: :home
 
   def home
   end
