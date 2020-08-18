85ddb814560f9a6417b76e4b3bcc91166ee1e759
Vincent Pacheco <vincentpacheco@outlook.com>
2020-08-18 11:14:42 +0200

    Modifications done.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,27 @@
 class BookingsController < ApplicationController
+  before_action :set_travel, only: [:new, :create]
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = booking.new(booking_params)
+    @booking.travel = @travel
+    if @booking.save
+      redirect_to travel_path(@travel)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :note)
+  end
+
+  def set_travel
+    @travel = Travel.find(params[:travel_id])
+  end
 end
