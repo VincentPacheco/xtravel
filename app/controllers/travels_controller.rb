cfe209c41fb5c9d6bbcda125d5e48b3742405506
Stéphane ZE-OGIER <contact@ze-ogier.com>
2020-08-18 11:40:00 +0200

    add travels controllers and views

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -4,4 +4,27 @@ class TravelsController < ApplicationController
     @travels = Travel.all
   end
 
+  def show
+    @travel = Travel.find(params[:id])
+  end
+
+  def new
+    @travel = Travel.new
+  end
+
+  def create
+    @travel = Travel.new(travel_params)
+    if @travel.save
+      redirect_to travel_path(@travel)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def travel_params
+    params.require(:travel).permit(:title, :description, :price)
+  end
+
 end
