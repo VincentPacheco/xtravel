d7886110cacbbe5d0622cd05727e4484fe8d35f0
Stéphane ZE-OGIER <contact@ze-ogier.com>
2020-08-18 14:35:46 +0200

    pundit added with some bug to fix on travels

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,25 +1,49 @@
 class TravelsController < ApplicationController
 
   def index
-    @travels = Travel.all
+    @travels = policy_scope(Travel).order(created_at: :desc)
   end
 
   def show
     @travel = Travel.find(params[:id])
+    authorize @travel
   end
 
   def new
-    @travel = Travel.new
+    @travel = user.travels.new
+    authorize @travel
   end
 
   def create
-    @travel = Travel.new(travel_params)
+    current_user.travels.new(travel_params)
+    authorize @travel
     @travel.user = current_user
     if @travel.save!
       redirect_to travel_path(@travel)
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @travel = Travel.find(params[:id])
+    authorize @travel
+    @travel.destroy
+    redirect_to travels_path
+  end
+
+  def edit
+    @travel = Travel.find(params[:id])
+    authorize @travel
+  end
+
+  def update
+    @travel = Travel.find(params[:id])
+    authorize @travel
+    @travel.update(travel_params)
+
+    # no need for app/views/travels/update.html.erb
+    redirect_to travel_path(@travel)
   end
 
   private
