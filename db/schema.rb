c499cabb647849cfbf9647408f51baa0af9d4b96
Vincent Pacheco <vincentpacheco@outlook.com>
2020-08-20 13:11:37 +0200

    Capacity model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_105749) do
+ActiveRecord::Schema.define(version: 2020_08_20_111030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_105749) do
     t.string "address"
     t.text "longdescription"
     t.integer "duration"
+    t.integer "capacity"
     t.index ["user_id"], name: "index_travels_on_user_id"
   end
 
