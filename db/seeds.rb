37ab999ff96d1fcadc24c9613826d57e1c55e472
Vincent Pacheco <vincentpacheco@outlook.com>
2020-08-21 15:51:37 +0200

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -89,8 +89,8 @@ puts'Generating travels'
     travel.title = planet
     travel.address = planet
     travel.price = price
-    travel.description = Faker::Games::WorldOfWarcraft.quote
-    travel.longdescription = Faker::Quote.yoda
+    travel.description = Faker::Games::Overwatch.quote
+    travel.longdescription = Faker::Games::WorldOfWarcraft.quote
     travel.duration = Faker::Number.within(range: 45..170)
     travel.capacity = Faker::Number.within(range: 40..250)
     travel.latitude = latitude
